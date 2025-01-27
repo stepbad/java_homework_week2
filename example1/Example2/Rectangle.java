468109b50c73d698654da329f12ed0c25ebf16c9
Stephen Badcock <badcockstephen73@gmail.com>
2025-01-27 19:38:58 -0330

    ex2
    completed ex2 rectangle problem

---
+++ example1/Example2/Rectangle.java
@@ -0,0 +1,51 @@
+package example1.Example2;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    // Constructor
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // Getters and Setters
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if (length > 0) {
+            this.length = length;
+        } else {
+            System.out.println("Length must be positive.");
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width > 0) {
+            this.width = width;
+        } else {
+            System.out.println("Width must be positive.");
+        }
+    }
+
+    // Methods to calculate area and perimeter
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    
+    public String toString() {
+        return "Rectangle [Length=" + length + ", Width=" + width + "]";
+    }
+}
---
+++ example1/Example2/TestRectangle.java
@@ -0,0 +1,27 @@
+package example1.Example2;
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        // Create two Rectangle objects using the constructor
+        Rectangle rect1 = new Rectangle(5.0, 4.0);
+        Rectangle rect2 = new Rectangle(6.5, 3.5);
+
+        // Display initial details of both rectangles
+        System.out.println(rect1);
+        System.out.println(rect2);
+
+        // Calculate and display the area and perimeter of both rectangles
+        System.out.println("Rectangle 1 - Area: " + rect1.calculateArea() + ", Perimeter: " + rect1.calculatePerimeter());
+        System.out.println("Rectangle 2 - Area: " + rect2.calculateArea() + ", Perimeter: " + rect2.calculatePerimeter());
+
+        // Modify the dimensions of rect1 using setters
+        rect1.setLength(7.0);
+        rect1.setWidth(5.0);
+
+        // Display updated details of rect1
+        System.out.println("Updated " + rect1);
+
+        // Recalculate and display the area and perimeter of rect1
+        System.out.println("Updated Rectangle 1 - Area: " + rect1.calculateArea() + ", Perimeter: " + rect1.calculatePerimeter());
+    }
+}

bf1fb1682854a0a7630d6fc48bec53262995bf61
Stephen Badcock <badcockstephen73@gmail.com>
2025-01-27 18:33:55 -0330

    Create Nurse.java
    example 1 nurse.java

---
+++ example1/Nurse.java
@@ -0,0 +1,19 @@
+public class Nurse {
+    private int id;
+    private String name;
+    private String specialization;
+
+    public Nurse(int id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public void assistDoctor(String task) {
+        System.out.println(name + " is assisting with: " + task);
+    }
+
+    public String toString() {
+        return "Nurse [ID=" + id + ", Name=" + name + ", Specialization=" + specialization + "]";
+    }
+}
